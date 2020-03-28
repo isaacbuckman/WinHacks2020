fdf7d6f0423ec31ce60a45ce7333a4036365481c
Isaac Buckman <isaacbuckman4@gmail.com>
2020-03-28 09:56:27 -0400

    create diff types of forms

--- app.py
+++ app.py
@@ -1,25 +1,15 @@
 from random import randint
 from time import strftime
 from flask import *
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField, SelectField
 from firebase import firebase
+from forms import *
 
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = 'SjdnUends821Jsdlkvxh391ksdODnejdDw'
 
-service_types = [
-					("Materials","Materials"),
-					("Equipment","Equipment"),
-					("Labor","Labor")
-				]
 
-class RegisterForm(Form):
-	name = TextField('Name:', validators=[validators.required()])
-	contact = TextField('Phone #:', validators=[validators.required()])
-	loc = TextField("Location:", validators=[validators.required()])
-	service_type = SelectField("What you have to offer:", choices=service_types,validators=[validators.required()])
 
 # def get_time():
 # 	time = strftime("%Y-%m-%dT%H:%M")
@@ -29,10 +19,10 @@ firebase = firebase.FirebaseApplication('https://winhacks2020-44957.firebaseio.c
 def add_new_biz (name, contact, loc, service_type):
 
 	data = {
-		"Name" : name,
-		"Phone" : contact,
-		"Location" : loc,
-		"Service" : service_type
+		"name" : name,
+		"phone" : contact,
+		"location" : loc,
+		"service" : service_type
 	}
 
 	result = firebase.post('/Users', data);
@@ -61,7 +51,7 @@ def register():
 	if form.validate():
 		name = add_new_biz(name, contact, loc, service_type)
 
-		flash('Hello: {}'.format(name))
+		# flash('Hello: {}'.format(name))
 		# return render_template('index.html', form=form)
 		return redirect(url_for('apply', id=name))
 
@@ -71,7 +61,18 @@ def register():
 
 @app.route('/apply/<id>')
 def apply(id):
-	return render_template('apply.html', id=find_biz(id))
+	biz_data = find_biz(id)
+	if biz_data['service'] == "Materials":
+		return render_template('apply_materials.html', name=biz_data['name'])
+	elif biz_data['service'] == "Equipment":
+		return render_template('apply_equipment.html', name=biz_data['name'])
+	elif biz_data['service'] == "Labor":
+		form = LaborForm(request.form)
+		return render_template('apply_labor.html', form=form, name=biz_data['name'])
+	else:
+		return render_template("error")
+
+	
 	# form = ApplyForm(request.form)
 
 if __name__ == "__main__":
---
+++ forms.py
@@ -0,0 +1,22 @@
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField, SelectField
+
+service_types = [
+					("Materials","Materials"),
+					("Equipment","Equipment"),
+					("Labor","Labor")
+				]
+
+class RegisterForm(Form):
+	name = TextField('Name:', validators=[validators.required()])
+	contact = TextField('Phone #:', validators=[validators.required()])
+	loc = TextField("Location:", validators=[validators.required()])
+	service_type = SelectField("What you have to offer:", choices=service_types,validators=[validators.required()])
+
+class LaborForm(Form):
+	quant = TextField('Number of People:')
+
+class MaterialsForm(Form):
+	yes= True
+
+class EquipmentForm(Form):
+	yes = True
