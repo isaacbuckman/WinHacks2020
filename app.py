90bc505635662809fc36d65fe429b224079bf6d9
Isaac Buckman <isaacbuckman4@gmail.com>
2020-03-28 14:15:52 -0400

    login screen forms for all

--- app.py
+++ app.py
@@ -23,77 +23,129 @@ def add_new_biz (name, contact, loc, service_type):
 	print(result)
 	return result['name']
 
-def add_labor(id, quant):
+def add_material(id, material, qty, delay):
+	data = {
+		"material" : material,
+		"qty" : qty,
+		"delay" : delay
+	}
+
+	result = firebase.post('/Users/' + id + '/Materials', data)
+
+def add_equipment(id, equipment, qty):
+	data = {
+		"equipment" : equipment,
+		"qty" : qty
+	}
+
+	result = firebase.post('/Users/' + id + '/Equipment', data)
+
+def add_labor(id, qty, quals):
 
 	data = {
-		"quant" : quant
+		"qty" : qty,
+		"sewing" : quals['sewing'],
+		"cooking" : quals['cooking']
 	}
 
 	result = firebase.post('/Users/' + id + '/Labor', data)
-	# firebase.put('/Users/' + id + '/Labor', "quant", quant)
+	# firebase.put('/Users/' + id + '/Labor', "qty", qty)
 
 def find_biz(id):
 	result = firebase.get('/Users', id)
 	return result
 
 @app.route("/", methods=['GET','POST'])
-def hello():
-	form = RegisterForm(request.form)
-	return render_template('index.html', form=form)
-
-@app.route("/register", methods=['POST'])
-def register():
-	form = RegisterForm(request.form)
-
-    #print(form.errors)
-	name=request.form['name']
-	contact=request.form['contact']
-	loc=request.form['loc']
-	service_type=request.form['service_type']
-
-	if form.validate():
-		name = add_new_biz(name, contact, loc, service_type)
-
-		# flash('Hello: {}'.format(name))
-		# return render_template('index.html', form=form)
-		return redirect(url_for('apply', id=name))
-
-	else:
-		flash('Error: All Fields are Required')
+def login():
+	if  request.method == 'GET':
+		form = LoginForm(request.form)
 		return render_template('index.html', form=form)
+
+	if request.method == 'POST':
+		return "handle login screen"
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+	if request.method == 'GET':
+		form = RegisterForm(request.form)
+		return render_template('register.html', form=form)
+
+	if request.method == 'POST':
+		form = RegisterForm(request.form)
+		name=request.form['name']
+		contact=request.form['contact']
+		loc=request.form['loc']
+		service_type=request.form['service_type']
+
+		if form.validate():
+			name = add_new_biz(name, contact, loc, service_type)
+
+			# flash('Hello: {}'.format(name))
+			# return render_template('index.html', form=form)
+			return redirect(url_for('apply', id=name))
+
+		else:
+			flash('Error: All Fields are Required')
+			return render_template('register.html', form=form)
 
 @app.route('/apply/<id>', methods=['GET','POST'])
 def apply(id):
 	biz_data = find_biz(id)
 
 	if request.method == 'GET' :
+		
 		if biz_data['service'] == "Materials":
-			return render_template('apply_materials.html', name=biz_data['name'])
-		elif biz_data['service'] == "Equipment":
-			return render_template('apply_equipment.html', name=biz_data['name'])
-		elif biz_data['service'] == "Labor":
+			form = MaterialsForm(request.form)
+			return render_template('apply_materials.html', form=form, id=id, name=biz_data['name'])
+		
+		if biz_data['service'] == "Equipment":
+			form = EquipmentForm(request.form)
+			return render_template('apply_equipment.html', form=form, id=id, name=biz_data['name'])
+		
+		if biz_data['service'] == "Labor":
 			form = LaborForm(request.form)
 			return render_template('apply_labor.html', form=form, id=id, name=biz_data['name'])
-		else:
-			return render_template("error")
 
 	if request.method == 'POST':
+
 		if biz_data['service'] == "Materials":
-			return render_template('apply_materials.html', name=biz_data['name'])
-		elif biz_data['service'] == "Equipment":
-			return render_template('apply_equipment.html', name=biz_data['name'])
-		elif biz_data['service'] == "Labor":
-			form = LaborForm(request.form)
-			quant=request.form['quant']
+			form = MaterialsForm(request.form)
+			material=request.form['material']
+			qty=request.form['qty']
+			delay=request.form['delay']
 
 			if form.validate():
-				add_labor(id, quant)
-				return quant
+				add_material(id, material, qty, delay)
+				return "" + material + " " + qty + " " + delay
+			else:
+				flash('Error: All Fields are Required')
+				return render_template('apply_materials.html', form=form, id=id, name=biz_data['name'])
+
+		if biz_data['service'] == "Equipment":
+			form = EquipmentForm(request.form)
+			equipment=request.form['equipment']
+			qty=request.form['qty']
+
+			if form.validate():
+				add_equipment(id, equipment, qty)
+				return "" + equipment + " " + qty
+			else:
+				flash('Error: All Fields are Required')
+				return render_template('apply_equipment.html', form=form, id=id, name=biz_data['name'])
+		
+		if biz_data['service'] == "Labor":
+			form = LaborForm(request.form)
+			qty=request.form['qty']
+			quals = {}
+			quals['sewing'] = form.sewing.data
+			quals['cooking'] = form.cooking.data
+
+			if form.validate():
+				add_labor(id, qty, quals)
+				return str(quals)
 			else:
 				flash('Error: All Fields are Required')
 				return render_template('apply_labor.html', form=form, id=id, name=biz_data['name'])
-		else:
-			return "error"
 
 if __name__ == "__main__":
 	app.run()
--- forms.py
+++ forms.py
@@ -1,4 +1,4 @@
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField, SelectField
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField, SelectField, BooleanField
 
 service_types = [
 					("Materials","Materials"),
@@ -6,17 +6,26 @@ service_types = [
 					("Labor","Labor")
 				]
 
+class LoginForm(Form):
+	name = TextField('Name:', validators=[validators.required()])
+	password = TextField('password:', validators=[validators.required()])
+
 class RegisterForm(Form):
 	name = TextField('Name:', validators=[validators.required()])
 	contact = TextField('Phone #:', validators=[validators.required()])
 	loc = TextField("Location:", validators=[validators.required()])
 	service_type = SelectField("What you have to offer:", choices=service_types,validators=[validators.required()])
 
-class LaborForm(Form):
-	quant = TextField('Number of People:', validators=[validators.required()])
-
 class MaterialsForm(Form):
-	yes= True
+	material = TextField('Material Name:', validators=[validators.required()])
+	qty = TextField('Quantity:', validators=[validators.required()])
+	delay = TextField('Time Delay:', validators=[validators.required()])
 
 class EquipmentForm(Form):
-	yes = True
+	equipment = TextField('Equipment Type:', validators=[validators.required()])
+	qty = TextField('Quantity:', validators=[validators.required()])
+
+class LaborForm(Form):
+	qty = TextField('Number of People:', validators=[validators.required()])
+	sewing = BooleanField("Sewing")
+	cooking = BooleanField("Cooking")
